71b2b51936190ccdaef7ff1a1fe84b89ab869013
pwan <pawel.wangryn@gfieast.com>
2021-08-12 11:51:17 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,28 @@
-// LIBRARIES
-
 #include <Arduino.h>
 #include <Hash.h>
 
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
 
 // Replace with your network credentials
-const char *ssid = "VAN24";
-const char *password = "vectraPawla24";
-
-// SETTINGS
-//setup
-const char *wlan_ssid = "VAN24";
-const char *wlan_password = "vectraPawla24";
-
-//end of setup
-// const String serial = "12345";  //(testowy)                 // musi byc zmieniony przy kazdym urzadzeniu
-const String serial = "12349"; //(biurko)
-// const char *ws_host = "192.168.0.21";           //adres serwera
-const char *ws_host = "192.168.0.11";
+const char *ssid = "van24";
+const char *password = "ruterPawla24";
+const String serial = "72340"; //(biurko)
+String ws_host = "192.168.1.214";
 const int ws_port = 9999;                        //port serwera
 const char *stompUrl = "/mywebsocket/websocket"; //stompowy endpoint
 //end of setup
 
-// URL for STOMP endpoint.
-// For the default config of Spring's STOMP support, the default URL is "/socketentry/websocket".
-// don't forget the leading "/" !!!
-
 int gpio_13_led = 13;
 int gpio_12_relay = 12;
 int gpio_12_button = 0;
+int gpio_s2_button = 4;
 
 // VARIABLES
 int buttonState = 0;
+int buttonState2 = 0;
 const char *status;
 const char *task;
 boolean is_on = false;
@@ -47,22 +35,10 @@ StaticJsonDocument<256> doc;
 WebSocketsClient webSocket;
 
 // FUNCTIONS
-
 void prepareLedUpdate(StaticJsonDocument<256> doc);
 void initialSetup(StaticJsonDocument<256> doc);
 // END OF FUNCTIONS
 
-// FUNCTIONS
-
-/**
- * STOMP messages need to be NULL-terminated (i.e., \0 or \u0000).
- * However, when we send a String or a char[] array without specifying 
- * a length, the size of the message payload is derived by strlen() internally,
- * thus dropping any NULL values appended to the "msg"-String.
- * 
- * To solve this, we first convert the String to a NULL terminated char[] array
- * via "c_str" and set the length of the payload to include the NULL value.
- */
 void sendMessage(String &msg)
 {
   webSocket.sendTXT(msg.c_str(), msg.length() + 1);
@@ -100,9 +76,6 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 
       String msg = "SUBSCRIBE\nid:sub-0\ndestination:/device/device/" + serial + "\n\n";
       sendMessage(msg);
-      // delay(1000);
-
-      // and send a message
     }
     else
     {
@@ -121,13 +94,7 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
       else
       {
         prepareLedUpdate(doc);
-        // Serial.write("check doesnt");
       }
-
-      // String msg = "SEND\ndestination:/device/initdevice/"+serial+"\n\n{\"user\":\"esp\",\"message\":\"Hello!\"}";
-      // sendMessage(msg);
-      // delay(10000);
-      // do something with messages
     }
 
     break;
@@ -135,9 +102,6 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
   case WStype_BIN:
     Serial.printf("[WSc] get binary length: %u\n", length);
     hexdump(payload, length);
-
-    // send data to server
-    // webSocket.sendBIN(payload, length);
     break;
   }
 }
@@ -174,23 +138,20 @@ void setup()
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.println(ws_host);
 
-  char str[12];
-  // sprintf(str, "%d", WiFi.localIP());
-  Serial.println(str);
   // connect to websocket
   webSocket.begin(ws_host, ws_port, stompUrl);
-  // webSocket.setExtraHeaders(); // remove "Origin: file://" header because it breaks the connection with Spring's default websocket config
-  // webSocket.setExtraHeaders("foo: I am so funny\r\nbar: not"); // some headers, in case you feel funny
   webSocket.onEvent(webSocketEvent);
 }
 
 void loop()
 {
   buttonState = digitalRead(gpio_12_button);
+  buttonState2 = digitalRead(gpio_s2_button);
   webSocket.loop();
   pressed = false;
-  if (buttonState == 0)
+  if (buttonState == 0 || buttonState2 == 0)
   {
 
     if (is_on == true && pressed == false)
