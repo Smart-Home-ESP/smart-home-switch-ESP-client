b33875f94bfd08139f47751c6594f1ff8c74896f
Paweł Wangryn <pwangryn@op.pl>
2021-10-17 16:05:31 +0200

    Add ota update support

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <EEPROM.h>
 #include <Hash.h>
 #include <FS.h>
+#include <ArduinoOTA.h>
 
 
 AsyncWebServer server(80);
@@ -221,12 +222,34 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
   }
 }
 
+void otaSetup(){
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+}
+
 void setup()
 {
   Serial.begin(115200);
   delay(5000);
   spiffsSetup();
   serverSetup();
+  otaSetup();
   //  Init
   pinMode(gpio_13_led, OUTPUT);
   digitalWrite(gpio_13_led, HIGH);
@@ -245,7 +268,7 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     digitalWrite(gpio_13_led, LOW);
-    delay(500);
+    delay(400);
     Serial.print(".");
     Serial.println(WiFi.localIP());
     Serial.println(WiFi.status());
@@ -370,6 +393,7 @@ void serverSetup(){
 
 void loop()
 {
+    ArduinoOTA.handle();
   buttonState2 = digitalRead(gpio_s2_button);
   webSocket.loop();
   if (pole == 1) {                   
